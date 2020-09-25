88b554a7eeb548f332de8cc479df65d620403a14
Henrique <henriquegundes@outlook.com>
2020-09-25 15:34:32 -0300

    PUT method

--- src/main/java/com/product/apirest/resources/ProductResource.java
+++ src/main/java/com/product/apirest/resources/ProductResource.java
@@ -6,9 +6,11 @@ import com.product.apirest.model.Product;
 import com.product.apirest.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,16 @@ public class ProductResource {
 
     @PostMapping("/product")
     public Product saveProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable(value="id") long id) {
+        productRepository.deleteById(id);
+    }
+
+    @PutMapping("/product")
+    public Product updateProduct(@RequestBody Product product) {
         return productRepository.save(product);
     }
 }
